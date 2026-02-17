d9758804b125bed6ee4cff6353f377c314569882
vidhi-me <vs5194@srmist.edu.in>
2026-02-17 15:32:55 +0530

    uc1

---
+++ OopsBannerApp.java
@@ -0,0 +1,10 @@
+public class OopsBannerApp {
+    public static void main(String[] args) {
+		System.out.println("OOPS");
+		System.out.println("**** **** **** ****");
+		System.out.println("*  * *  * *  * *   ");
+		System.out.println("*  * *  * **** ****");
+		System.out.println("*  * *  * *       *");
+		System.out.println("**** **** *    ****");
+	}
+}
